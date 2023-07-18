ad9250724bc7535c9e9feac7e7542da6443860db
Kinder-Pl <lilimakiavelli@gmail.com>
2023-07-18 23:28:16 +0300

    Initial commit

---
+++ UniqueLetter.py
@@ -0,0 +1,33 @@
+def unique_symbol(word):
+    frequency = dict()
+    for letter in word:
+        if letter not in frequency:
+            frequency[letter] = 1
+        else:
+            frequency[letter] += 1
+    for symbol in word:
+        if frequency[symbol] == 1:
+            return symbol
+
+
+def unique_letter_from_list(letters_list):
+    frequency = dict()
+    for letter in letters_list:
+        if letter not in frequency:
+            frequency[letter] = 1
+        else:
+            frequency[letter] += 1
+    for symbol in letters_list:
+        if frequency[symbol] == 1:
+            return symbol
+
+def get_unique_letter(text):
+    translating = str.maketrans(".,\n-!?:''", "         ")
+    parsed_text = text.translate(translating)
+    words_and_spaces = []
+    words_and_spaces.extend(parsed_text.split(" "))
+    words = [word for word in words_and_spaces if word.isalpha()]
+    list_unique_letter = []
+    for word in words:
+        list_unique_letter.append(unique_symbol(word))
+    return unique_letter_from_list(list_unique_letter)
---
+++ gui.py
@@ -0,0 +1,49 @@
+def main():
+    import tkinter as tk
+    from UniqueLetter import get_unique_letter
+    import customtkinter as ctk
+
+
+
+    def button_func():
+        text = main_textbox.get(1.0, "end-1c")
+        var.set(get_unique_letter(text))
+
+
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+
+    window = ctk.CTk()
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_columnconfigure(0, weight=1)
+    window.title("Uniqueness")
+    window.geometry("500x400")
+
+    winWidth = window.winfo_reqwidth()
+    winHeight = window.winfo_reqheight()
+    posRight = int(window.winfo_screenwidth() / 2 - winWidth / 2)
+    posDown = int(window.winfo_screenheight() / 2 - winHeight / 2)
+    # window.geometry("+{}+{}".format(posRight, posDown))
+
+    main_label = ctk.CTkLabel(master=window,
+                              text="Type any text to get first unique symbol",
+                              font=('Arial', 18))
+    main_label.grid(pady=25, row=0, column=0)
+
+    var = tk.StringVar(window, "Your answer will be here")
+
+    main_textbox = tk.Text(window, highlightthickness=0, width=1, height=5, padx=10, pady=10)
+    main_textbox.grid(row=1, column=0, sticky="nsew", padx=100)
+
+
+    ctk_textbox_scrollbar = ctk.CTkScrollbar(window, command=main_textbox.yview)
+    ctk_textbox_scrollbar.grid(row=1, column=1, sticky="ns")
+    main_textbox.configure(yscrollcommand=ctk_textbox_scrollbar.set)
+
+    answer_label = ctk.CTkLabel(master=window,  textvariable=var, font=("Arial", 20))
+    answer_label.grid()
+
+    main_button = ctk.CTkButton(master=window, text="Find unique symbol", font=("Arial", 16), command=button_func)
+    main_button.grid(pady=20)
+
+    window.mainloop()
